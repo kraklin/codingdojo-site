d6d89f57232886cf4f12c0725dbf7d885ef92068
jirkapenzes <jirkapenzes@gmail.com>
2015-06-10 14:44:19 +0200

    Added production mode

--- project.clj
+++ project.clj
@@ -12,4 +12,5 @@
   :profiles
 
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+  {:production {:env {:production true}}})
